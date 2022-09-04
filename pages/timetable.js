d80dce6bacddcfe1f06ce7a0793f015ab67deeed
Arsenii Kopaev <senya.kopaev@mail.ru>
2022-09-04 21:30:14 +0300

    base layout

---
+++ pages/timetable.js
@@ -0,0 +1,45 @@
+import styles from "../styles/timetable.module.css"
+
+const timetable = () => {
+    return (
+        <>
+            <header className={styles.header}>
+                <h1 className={styles.header__title}>Stubtable</h1>
+            </header>
+            <main>
+                <h2 className={styles.main__title}>Понедельник, 5 сентября</h2>
+                <div className={styles.timetable__wrapper}>
+                    <div className={styles.timetable__item}>
+                        <div>8:00-9:35</div>
+                        <div>
+                            <span>Мат. Анализ</span>
+                            <br></br>
+                            <span>Платенова О.К.</span>
+                        </div>
+                        <div>436</div>
+                    </div>
+                    <div className={styles.timetable__item}>
+                        <div>8:00-9:35</div>
+                        <div>
+                            <span>Мат. Анализ</span>
+                            <br></br>
+                            <span>Платенова О.К.</span>
+                        </div>
+                        <div>436</div>
+                    </div>
+                    <div className={styles.timetable__item}>
+                        <div>8:00-9:35</div>
+                        <div>
+                            <span>Мат. Анализ</span>
+                            <br></br>
+                            <span>Платенова О.К.</span>
+                        </div>
+                        <div>436</div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default timetable;
