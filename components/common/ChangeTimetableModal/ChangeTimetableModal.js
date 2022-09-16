21f276e3683235caa1b9aa4bb2a41d73dc0cd952
Arsenii Kopaev <senya.kopaev@mail.ru>
2022-09-16 19:46:24 +0300

    made multichanging system for timetable

--- components/common/ChangeTimetableModal/ChangeTimetableModal.js
+++ components/common/ChangeTimetableModal/ChangeTimetableModal.js
@@ -1,51 +1,19 @@
 import styles from "../ChangeTimetableModal/ChangeTimetableModal.module.css"
 
-import { useFormik } from "formik";
-import * as Yup from 'yup';
 import useHttp from "../../../hooks/useHttps";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 const parameters = require('../../../parameters.json')
-const tableTypes = require('../../../pages/static/TableTypes.json')
-import { discliplinesFetching, discliplinesFetched, discliplinesFetchingError, timetableFetching, timetableFetched, timetableFetchingError } from '../../../actions/actions'
 
-const ChangeTimetableModal = ({ isVisiable, onClose, lessionNum, fetchTimetable}) => {
+import { discliplinesFetching, discliplinesFetched, discliplinesFetchingError } from '../../../actions/actions'
+
+const ChangeTimetableModal = ({ isVisiable, onClose, lessionNum, formik, modalType, setModalType}) => {
     const { request } = useHttp();
-    const { discliplines, selectedDay, timetable } = useSelector(state => state.reducer)
+    const { discliplines, timetable } = useSelector(state => state.reducer)
     const dispatch = useDispatch();
+    
 
-    const formik =  useFormik({
-        initialValues: {
-            subject: '', 
-            teacher: '',
-            audience: '' 
-        },
-        validationSchema: Yup.object({
-            subject: Yup.string().required('Выберите предмет!'),
-            teacher: Yup.string().required('Выберите преподавателя!'),
-            audience: Yup.string().required('Введите аудиторию!')
-        }), 
-        onSubmit: values => {
-            let body = {day: selectedDay, 
-                type: tableTypes.PERMANENT, 
-                changes: {}
-            }
-            body.changes[lessionNum] = {
-                audience: values.audience,
-                lessionName: values.subject, 
-                teacher: values.teacher,
-                type: tableTypes.PERMANENT
-            }
-
-            request(`${parameters.API_HOST}/updatetable`, 'POST', JSON.stringify(body))
-                .then(() => onClose()) 
-                .then(() => fetchTimetable())
-            formik.values.subject = ''
-            formik.values.teacher = ''
-            formik.values.audience = ''
-        }
-    })  	    
-
+    
     useEffect(() => {
         if (isVisiable && discliplines === ''){
             dispatch(discliplinesFetching())
@@ -73,7 +41,7 @@ const ChangeTimetableModal = ({ isVisiable, onClose, lessionNum, fetchTimetable}
 
     return !isVisiable ? null :
     (
-    <div className={styles.Modal} /* onClick={onClose} */>
+    <div className={styles.Modal} key={lessionNum} /* onClick={onClose} */>
         <div className={styles.Modal__dialog}>
             <div className={styles.Modal__header}>
                 <span className={styles.Modal__title}>Изменить пару {timetable[lessionNum].time}</span>
@@ -82,25 +50,38 @@ const ChangeTimetableModal = ({ isVisiable, onClose, lessionNum, fetchTimetable}
             <div className={styles.Modal__content}>
                 <form onSubmit={formik.handleSubmit}>
 
-                    <select name="subject" value={formik.subject} onChange={formik.handleChange} onBlur={formik.handleBlur}>
-                        <option value="">Выберите предмет</option>
-                        {discliplinesOptions}
-                    </select>
+                    <div>
+                        <button onClick={() => setModalType('default')} type='button'>Обычный</button>
+                        <button onClick={() => setModalType('special') }type='button'>Числитель/знаменатель</button>
+                    </div>
 
-                    {formik.values.subject ? (
-                        <select name="teacher" value={formik.values.teacher} onChange={formik.handleChange} onBlur={formik.handleBlur}>
-                            <option value="">Выберите преподователя</option>
-                            {teacherOptions}
-                        </select>
+                    {modalType === 'default' ? (
+                        <>
+                            <select name="subject" value={formik.subject} onChange={formik.handleChange} onBlur={formik.handleBlur}>
+                                <option value="">Выберите предмет</option>
+                                {discliplinesOptions}
+                            </select>
+                            {formik.errors.subject && formik.touched.subject ? <div>{formik.errors.subject}</div> : null}
+
+                            <select name="teacher" value={formik.values.teacher} onChange={formik.handleChange} onBlur={formik.handleBlur}>
+                                <option value="">Выберите преподователя</option>
+                                {teacherOptions}
+                            </select>
+                            {formik.errors.teacher && formik.touched.teacher ? <div>{formik.errors.teacher}</div> : null}
+
+                            <input name="audience"  placeholder="Введите номер аудитории" value={formik.values.audience} onChange={formik.handleChange} onBlur={formik.handleBlur}></input>
+                            {formik.errors.audience && formik.touched.audience ? <div>{formik.errors.audience}</div> : null}
+
+                            <button type='submit'>Изменить</button>
+                        </>
                     ) : null}
 
-                    {formik.values.subject && formik.values.teacher ? (
-                        <input name="audience"  placeholder="Введите номер аудитории" value={formik.values.audience} onChange={formik.handleChange} onBlur={formik.handleBlur}></input>
+                    {modalType === 'special' ? (
+                        <>
+                            Spicial from 
+                        </>
                     ) : null}
-
-                    {formik.values.subject && formik.values.teacher && formik.values.audience ? (
-                        <button type='submit'>Изменить</button>
-                    ) : null}
+                    
                 </form>
             </div>
         </div>
--- pages/createPermanent.js
+++ pages/createPermanent.js
@@ -10,18 +10,83 @@ import styles from "../styles/pages/createPermanent.module.css"
 import Layout from "../components/Layout/Layout";
 import checkPermissons from "../functions/checkPermissons"
 import Spinner from "../components/major/Spinner/Spinner";
+import { useFormik } from "formik";
+import * as Yup from 'yup';
+const tableTypes = require('../pages/static/TableTypes.json')
+
 
 const createPermanent = () => {
+
+
+
     const [isVisiable, setIsVisiable] = useState(false);
     const [lessionNum, setLessionNum] = useState();
     const {selectedDay, timetable, timetableStatus} = useSelector(state => state.reducer);
     const dispatch = useDispatch();
+    const [changes, setCanges] = useState({});
+    const [modalType, setModalType] = useState('');
+
+
+    const formik =  useFormik({
+        initialValues: {
+            subject: '', 
+            teacher: '',
+            audience: '' 
+        },
+        validationSchema: Yup.object({
+            subject: Yup.string().required('Выберите предмет!'),
+            teacher: Yup.string().required('Выберите преподавателя!'),
+            audience: Yup.string().required('Введите аудиторию!')
+        }), 
+        onSubmit: values => {
+            let changeObj = {};
+            changeObj[lessionNum] = {
+                audience: values.audience,
+                lessionName: values.subject, 
+                teacher: values.teacher,
+                type: tableTypes.PERMANENT  
+            }
+
+            setCanges(changes => ({
+                ...changes, 
+                ...changeObj
+            }))
+
+            formik.values.subject = ''
+            formik.values.teacher = ''
+            formik.values.audience = ''
+
+            onClose();
+        }
+       
+
+    })  	 
+
+
+    const onClear = () => {
+        setCanges({});
+    }
+    const onSend = () => {
+        let body = {
+            day: selectedDay,
+            type: tableTypes.PERMANENT, 
+            changes: changes
+        }
+        request(`${parameters.API_HOST}/updatetable`, 'POST', JSON.stringify(body))
+                .then((result) => {
+                        setCanges({});
+                        fetchTimetable();
+                })/* .catch(() => ) */
+    }
+
     const onOpen = (i) => {
         setIsVisiable(true)
         setLessionNum(i)
     }
     const onClose = () => {
         setIsVisiable(false)
+        formik.resetForm() 
+        setModalType('')
     };
 
     const { request } = useHttp();
@@ -46,19 +111,33 @@ const createPermanent = () => {
     }, [selectedDay])
     
     let timetableList = Object.keys(timetable).map((item, i) => {
-        return (
-            <>
-                <div key={i} onClick={() => onOpen(i)} className={styles.timetableItem__wrapper}>
+
+        if(changes.hasOwnProperty(item)){
+            return(
+                <div key={i} onClick={() => onOpen(i)} className={styles.changedTimetableItem__wrapper}>
                     <div className={styles.time__wrapper}>{timetable[item].time}</div>
                     <div className={styles.textItems__wrapper}>
-                        <span className={styles.lessionName}>{timetable[item].lessionName}</span>
+                        <span className={styles.lessionName}>{changes[item].lessionName}</span>
                         <br></br>
-                        <span>{timetable[item].teacher}</span>
+                        <span>{changes[item].teacher}</span>
                     </div>
-                    <div>{timetable[item].audience}</div>
-                    {timetable[item].type === null ? <p></p> : null}
+                    <div>{changes[item].audience}</div>
+                    {changes[item].type === null ? <p></p> : null}
                 </div>
-            </>
+            )
+        }
+
+        return (
+            <div key={i} onClick={() => onOpen(i)} className={styles.timetableItem__wrapper}>
+                <div className={styles.time__wrapper}>{timetable[item].time}</div>
+                <div className={styles.textItems__wrapper}>
+                    <span className={styles.lessionName}>{timetable[item].lessionName}</span>
+                    <br></br>
+                    <span>{timetable[item].teacher}</span>
+                </div>
+                <div>{timetable[item].audience}</div>
+                {timetable[item].type === null ? <p></p> : null}
+            </div>
         )
     })
 
@@ -80,7 +159,11 @@ const createPermanent = () => {
                 <h3 className={styles.createPermanent__title}>Обновление расписания:</h3>
                 <DaySelector />
                 {timetableList}
-                <ChangeTimetableModal isVisiable={isVisiable} onClose = {onClose} lessionNum={lessionNum} fetchTimetable={fetchTimetable}></ChangeTimetableModal>
+                <ChangeTimetableModal setModalType={setModalType} modalType={modalType} formik={formik} isVisiable={isVisiable} onClose = {onClose} lessionNum={lessionNum} fetchTimetable={fetchTimetable}></ChangeTimetableModal>
+                <div>
+                    <button onClick={onSend}>Сохранить</button>
+                    <button onClick={onClear}>Отмнеить</button>
+                </div>
             </div>
         </>
     )
