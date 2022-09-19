b97d5c52968ae54f810d0d30b5ee82f8ee29ec06
Arsenii Kopaev <senya.kopaev@mail.ru>
2022-09-19 21:53:39 +0300

    made logout

--- pages/profile.js
+++ pages/profile.js
@@ -6,6 +6,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { userFetching, userFetched, userFetchingError } from "../actions/actions";
 import styles from "../styles/pages/profile.module.css"
 import Spinner from "../components/major/Spinner/Spinner";
+import Link from "next/link";
 
 import checkLogin from "../functions/checkLogin";
 
@@ -14,19 +15,24 @@ const profile = () => {
     const {request} = useHttp()
     const {user, userStatus} = useSelector(state => state.reducer)
 
+    const logOut = () => {
+        request(`${parameters.API_HOST}/exitaccount`)
+    }
+
     useEffect(() => {
         checkLogin();
     }, [])
+
     useEffect(() => {
         if (user === ''){
             dispatch(userFetching())
             request(`${parameters.API_HOST}/getuserinfo`)
-                .then(response => dispatch(userFetched(response.data)))
+                .then(response => response.statusCode === '100' ? dispatch(userFetched(response.data)) : dispatch(userFetchingError()))
                 .catch(() => dispatch(userFetchingError()))
         }
     }, [])
     
-    if (userStatus === 'loading'){
+    if (userStatus === 'loading' || user === ''){
         return (
             <>
                 <Layout />
@@ -34,21 +40,24 @@ const profile = () => {
             </>
         )
     }
-    return (
-        <> 
-            <Layout></Layout>
-            <div className={styles.wrapper}>
-                <h3>{user.realInfo?.firstName} {user.realInfo?.secondName}</h3>
-                {user?.isConfirmed ? <p className={styles.green}>Подтвержденный</p> : <p className={styles.red}>Не подтвержденный</p>} 
-                {user?.accountType === "GroupHeadman" ? <p>Роль: староста</p> : <p>Роль: студент</p>}
-                <p>Факультет: {user.academy?.faculty}</p>
-                <p>Курс: {user.academy?.course}</p>
-                <p>Группа: {user.academy?.group}</p>
-                <p>Направление: {user.academy?.directionId}</p>
-                <p className={styles.logout}>Выйти</p>
-            </div>
-        </>
-    )
+    
+    if (userStatus === 'idle'){
+        return (
+            <> 
+                <Layout></Layout>
+                <div className={styles.wrapper}>
+                    <h3>{user?.realInfo?.firstName} {user?.realInfo?.secondName}</h3>
+                    {user?.isConfirmed ? <p className={styles.green}>Подтвержденный</p> : <p className={styles.red}>Не подтвержденный</p>} 
+                    {user?.accountType === "GroupHeadman" ? <p>Роль: староста</p> : <p>Роль: студент</p>}
+                    <p>Факультет: {user?.academy?.faculty}</p>
+                    <p>Курс: {user?.academy?.course}</p>
+                    <p>Группа: {user?.academy?.group}</p>
+                    <p>Направление: {user?.academy?.directionId}</p>
+                    <Link href='/login'><p className={styles.logout} onClick={logOut}>Выйти</p></Link>
+                </div>
+            </>
+        )
+    }
 }
 
 export default profile;
