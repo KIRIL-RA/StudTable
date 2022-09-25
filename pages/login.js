2a6f3928412d7c445de132db9b0ae0e65332b259
Arsenii Kopaev <senya.kopaev@mail.ru>
2022-09-25 17:24:21 +0300

    link to registration

--- pages/login.js
+++ pages/login.js
@@ -4,6 +4,7 @@ import Router from 'next/router';
 import { useState, useEffect } from 'react';
 const parameters = require('../parameters.json')
 import styles from '../styles/pages/login.module.css'
+import Link from 'next/link';
 
 const login = () => {
     const [serverError, setServerError] = useState('');
@@ -41,16 +42,18 @@ const login = () => {
 
     return(
         <div className={styles.login__wrapper}>
-            <h1 className={styles.login__title}>StubTable!</h1>
+            <h1 className={styles.login__title}>StudTable!</h1>
             <form onSubmit={formik.handleSubmit} className={styles.form__wrapper}>
                 <input className={styles.form__input} type="text" value={formik.values.username} onChange={formik.handleChange} onBlur={formik.handleBlur} name="username" placeholder='Имя пользователя'></input>
                 {formik.errors.username && formik.touched.username ? <div className={styles.form__error}>{formik.errors.username}</div> : null}
 
-                <input className={styles.form__input} type="text" value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} name="password" placeholder='Пароль'/>
+                <input className={styles.form__input} type="password" value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} name="password" placeholder='Пароль'/>
                 {formik.errors.password && formik.touched.password ? <div className={styles.form__error}>{formik.errors.password}</div> : null}
 
                 {serverError !== '' ? <div className={styles.form__error}>{serverError}</div> : null}
                 <button type="submit" className={styles.form__button}>Войти</button>
+
+                <Link href='/registration'><span className={styles.form__registration}>Регистрация</span></Link>
             </form>
         </div>
     )
